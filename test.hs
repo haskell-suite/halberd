c42dae3a1b8f772b34b32497b893f6f14bf29420
Erik Hesselink <hesselink@gmail.com>
2013-08-31 19:39:32 +0200

    Include second part of test case.

--- test.hs
+++ test.hs
@@ -5,8 +5,6 @@ main = do
     forM_ [1,2] $ \x -> print $ M.lookup x table
     return $ headNote "Impossible" [10]
 
-{-
 table :: M.Map String Int8
 table = M.fromList [("Odeca", 1), ("Hackathon",2)]
--}
 
